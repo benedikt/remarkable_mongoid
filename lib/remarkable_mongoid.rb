8076483be9f6da20ba90156a0c836c42d5c1cfc2
Brian Cardarella <bcardarella@gmail.com>
2010-07-10 17:10:04 -0400

    Auto include for RSpec

--- lib/remarkable_mongoid.rb
+++ lib/remarkable_mongoid.rb
@@ -6,3 +6,13 @@ end
 require 'remarkable/active_model'
 require 'remarkable_mongoid/associations'
 require 'remarkable_mongoid/validate_uniqueness_of'
+
+if defined?(RSpec) # RSpec 2.x
+  RSpec.configure do |config|
+    config.include(Remarkable::Mongoid::Matchers)
+  end
+elsif defined?(Spec) # RSpec 1.x
+  Spec::Runner.configure do |config|
+    config.include(Remarkable::Mongoid::Matchers)
+  end
+end
